c13789916caa2aeec6d34f0792d99b2aead10373
julianatonu <julianatonu@gmail.com>
2026-02-18 15:49:16 +0600

    add pick

---
+++ src/app/helper/pick.ts
@@ -0,0 +1,17 @@
+const pick = <T extends Record<string, unknown>, K extends keyof T>(
+  obj: T,
+  keys: K[]
+): Partial<T> => {
+
+  const finalObject: Partial<T> = {};
+
+  for (const key of keys) {
+    if (key in obj) {
+      finalObject[key] = obj[key];
+    }
+  }
+
+  return finalObject;
+};
+
+export default pick;
