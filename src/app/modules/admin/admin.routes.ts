e6a1e101ab89a33c152b13243b3e2d735dd22c97
julianatonu <julianatonu@gmail.com>
2026-02-19 11:50:34 +0600

    add auth in  doctorroutes and adminroutes

--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -2,10 +2,13 @@ import express, { Response, Request, NextFunction } from 'express';
 import { AdminController } from './admin.controller';
 import { fileUploader } from '../../helper/fileUploader';
 import { createAdminValidationSchema } from './admin.validation';
+import auth from '../../middlewares/auth';
+import { UserRole } from '@prisma/client';
 
 const router = express.Router();
 
 router.post("/create-admin",
+    auth(UserRole.ADMIN),
     fileUploader.upload.single("file"),
     (req: Request, res: Response, next: NextFunction) => {
         req.body = createAdminValidationSchema.parse(JSON.parse(req.body.data))
--- src/app/modules/doctor/doctor.routes.ts
+++ src/app/modules/doctor/doctor.routes.ts
@@ -2,12 +2,14 @@ import express, { NextFunction, Request, Response } from 'express';
 import { fileUploader } from '../../helper/fileUploader';
 import { createDoctorValidationSchema } from './doctor.validation';
 import { DoctorController } from './doctor.controller';
+import { UserRole } from '@prisma/client';
+import auth from '../../middlewares/auth';
 
 
 const router = express.Router()
 
 router.post("/create-doctor",
-
+    auth(UserRole.ADMIN),
     fileUploader.upload.single("file"),
     (req:Request, res:Response, next:NextFunction) =>{
         req.body = createDoctorValidationSchema.parse(JSON.parse(req.body.data))
